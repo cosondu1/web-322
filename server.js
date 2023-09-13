86dffc2f26ea7f58e3d5b21f90ef4cc42f1f27cb
Chidera Osondu <cosondu1@mySeneca.ca>
2023-09-13 13:28:08 -0400

    cyclic url added

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/********************************************************************************** 
+ * WEB322 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *  Name: _Chidera Osondu_______ Student ID: 174098210___ Date: _13/09/2023_
+ *  Online (Cyclic) URL: https://better-eel-blazer.cyclic.app/
+ * *********************************************************************************
+    
+    */
+
+
 import express  from "express";
 
 const app = express();
