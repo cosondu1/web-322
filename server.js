01bfe41723419cdae678f8ad8dbf90eb30b4a824
Chidera Osondu <cosondu1@mySeneca.ca>
2023-09-13 12:41:10 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+import express  from "express";
+
+const app = express();
+const HTTP_PORT = 3000;
+
+app.get("/", (req,res)=>{ res.send("Chidera Osondu - 174098210")}
+   
+)
+
+app.listen(HTTP_PORT, ()=>console.log(`server running on port ${HTTP_PORT}`))
+
